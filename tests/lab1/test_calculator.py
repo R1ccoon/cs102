907b4885b1b0fe60ea86b434d69987c37296cf08
Семён Фоченков <71787435+R1ccoon@users.noreply.github.com>
2023-10-25 20:25:59 +0300

    Update test_calculator.py
    unittets

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,32 @@
 import unittest
+from main import calculator
 
-class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+class TestCalculator(unittest.TestCase):
+    # setUp method is overridden from the parent class TestCas
+
+    # Each test method starts with the keyword test_
+    def test_add(self):
+        self.assertEqual(calculator('4+7'), 11)
+
+    def test_subtract(self):
+        self.assertEqual(calculator('7-3'), 4)
+
+    def test_multiply(self):
+        self.assertEqual(calculator('3*7'), 21)
+
+    def test_divide(self):
+        self.assertEqual(calculator('10//5'), 2)
+
+    def test_bad_input(self):
+        m = ['exit()', 'akasdkas', '01e2ke0', 'qdl']
+        for i in m:
+            self.assertEqual(calculator(i), 'Неверный формат ввода')
+
+    def test_zero_division(self):
+        self.assertEqual(calculator('10/0'), 'Деление на ноль')
+
+
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+    unittest.main()
